5aab3c59d7d20025c0dc4f6f41850cbe6cbe4b3b
JonBacc <jonathan.back99@gmail.com>
2022-02-11 14:47:10 +0200

    Added error handling. Able to run prospects now to 100%

--- src/main/java/Mortage_Plan.java
+++ src/main/java/Mortage_Plan.java
@@ -11,33 +11,51 @@
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
-import java.lang.Object; 
-import java.util.ArrayList;//Used for ArrayList<>
-import java.util.List;//Used for List<>
+import java.util.ArrayList; //Used for ArrayList<>
+import java.util.Arrays;
+import java.text.DecimalFormat;
 
 public class Mortage_Plan {
-    public static int outerCounter = 0;
+
     public static void main(String[] args) {
-        
+     int rows=0;
+     int outerCounter=0;
+     ArrayList<ArrayList<String>> prospects = new ArrayList<ArrayList<String>>(); 
+     
     try {
       File myObj = new File("src/main/resources/prospects.txt");
       Scanner myReader = new Scanner(myObj, "UTF-8");
       
-      //String header = myReader.nextLine();
-      //System.out.println(header);
-      myReader.nextLine();
-      while (myReader.hasNextLine()) {
+       while (myReader.hasNextLine()) {
+       String data = myReader.nextLine();
+       
+       if (data.length() > 1) {
+            if (data.startsWith("\"")) {
+                data = data.replace("\"","");
+                data = data.replaceFirst("(?:,)+", " ");
+            }
+            
         
-        String data = myReader.nextLine();
-        //System.out.println(data);
-        if(data.length()>1){
+       
+        ArrayList<String> line = new ArrayList<String>();     
+        String[] items = data.split(",");
+
+        for (int i = 0; i < items.length; i++) {
+            line.add(items[i]);
+        }
+        
+        prospects.add(line);
+        rows++;
+        Arrays.fill(items, null); // to clear out the 'items' array
+       }
+      }
+       
+       while(outerCounter<rows-1){
             outerCounter++;
-            String[] prospects = data.split(",");
-//            System.out.println(prospects[0]);
-            String name = String.valueOf(prospects[0]);
-            Float total_loan = Float.valueOf(prospects[1]);
-            Float interest = Float.valueOf(prospects[2]);
-            Float years = Float.valueOf(prospects[3]);
+            String name = String.valueOf(prospects.get(outerCounter).get(0));
+            Float total_loan = Float.valueOf(prospects.get(outerCounter).get(1));
+            Float interest = Float.valueOf(prospects.get(outerCounter).get(2));
+            Float years = Float.valueOf(prospects.get(outerCounter).get(3));
             Float months = years * 12;
             Float monthly_interest = interest / months;
             Float result = 1.0f;
@@ -48,32 +66,32 @@ public class Mortage_Plan {
                 // each multiplication
                 months--;
             }
+
             
-//            E = U[b(1 + b)^p]/[(1 + b)^p - 1]
-//            E = Fixed monthly payment
-//            b = Interest on a monthly basis
-//            U = Total loan
-//            p = Number of payment
-            Float monthlypayment = (total_loan*monthly_interest*result)/(result-1);
-            
-            
+            float monthlypayment = (total_loan*monthly_interest*result)/(result-1);
+            DecimalFormat f = new DecimalFormat("##.00");
+            DecimalFormat e = new DecimalFormat("##");
+   
             System.out.println("Prospect " + outerCounter + ": " 
                     +  name
                     + " wants to borrow " 
-                    + total_loan
+                    + (f.format(total_loan))
                     + " € for a period of "
-                    + years
+                    + (e.format(years))
                     + " years and pay "
-                    + monthlypayment
-                    + " each month ");
-        }
+                    + (f.format(monthlypayment))
+                    + "€ each month ");
         
-      }
+       }
+       
       myReader.close();
-    } catch (FileNotFoundException e) {
+    } 
+    
+    catch (FileNotFoundException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
     }
-        
+    
+    
   }
 }
